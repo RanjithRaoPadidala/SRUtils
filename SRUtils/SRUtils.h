24c91542b40a853f7cf71187f897147226a40dd9
ranjith <padidalaranjith@gmail.com>
2023-11-17 18:33:48 +0530

    Initial Commit

---
+++ SRUtils/SRUtils.h
@@ -0,0 +1,18 @@
+//
+//  SRUtils.h
+//  SRUtils
+//
+//  Created by Ranjith Padidala on 17/11/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SRUtils.
+FOUNDATION_EXPORT double SRUtilsVersionNumber;
+
+//! Project version string for SRUtils.
+FOUNDATION_EXPORT const unsigned char SRUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SRUtils/PublicHeader.h>
+
+
